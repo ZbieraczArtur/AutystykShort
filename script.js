bb9dd85de2a78b8e77bfd236015fc6bfa9971c74
ZbieraczArtur <arturzbieracz@gmail.com>
2026-05-19 21:01:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -357,7 +357,7 @@ function computeAndDisplayResults() {
   const { pairResults, ideologyResults, partyResults } = computeScores();
 
   // Wyświetl pary wartości
-  valuesResults.innerHTML = '<h3>⚖️ Wartości;
+  valuesResults.innerHTML = '<h3>⚖️ Pary wartości (przeciąganie liny)</h3>';
   pairResults.forEach(pair => {
     const pairDiv = document.createElement('div');
     pairDiv.className = 'value-pair';
